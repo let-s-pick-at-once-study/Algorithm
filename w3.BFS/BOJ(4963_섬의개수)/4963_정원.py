73f7aa590543d9f2cf2b4f4fde15cac4916d16ba
bluewhale0528 <47441767+bluewhale0528@users.noreply.github.com>
2021-01-03 01:51:09 +0900

    Update 4963_정원.py

--- w3.BFS/BOJ(4963_섬의개수)/4963_정원.py
+++ w3.BFS/BOJ(4963_섬의개수)/4963_정원.py
@@ -1,37 +1,32 @@
 import sys
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10000)
 
-# 방향(좌,우,위,아래 + 대각선)
-dx = [-1, 1, 0, 0, -1, 1, -1, 1]
-dy = [0, 0, -1, 1, -1, 1, 1, -1]
+dx = [1,-1,0,0,1,-1,1,-1]
+dy = [0,0,1,-1,1,-1,-1,1]
 
-def dfs(x,y):
-    visit[x][y] = True
+def dfs(x, y):
+    visited[x][y] = True
 
     for i in range(8):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if (0 <= nx < h) and (0 <= ny < w) and (not visit[nx][ny]) and (graph[nx][ny] == 1):
-            graph[nx][ny] = 0
+        if (0 <= nx < h) and (0 <= ny < w) and (not visited[nx][ny]) and (s[nx][ny] == 1):
             dfs(nx, ny)
 
-while (True):
-    w, h = map(int, sys.stdin.readline().split())
 
-    # 종료 조건
+while True:
+    w, h = map(int, sys.stdin.readline().split())
     if w == 0 and h == 0:
         break
 
-    visit = [ [False] * w for _ in range(h) ]
-    graph = []
-    for _ in range(h):
-        graph.append(list(map(int, sys.stdin.readline().split())))
+    s = [list(map(int, sys.stdin.readline().split())) for _ in range(h)]
+    visited = [[False] * w for _ in range(h)]
 
     count = 0
     for i in range(h):
         for j in range(w):
-            if not visit[i][j] and graph[i][j] == 1:
+            if (not visited[i][j]) and (s[i][j] == 1):
                 dfs(i, j)
                 count += 1
     print(count)
