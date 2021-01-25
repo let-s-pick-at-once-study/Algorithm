81a0e60c377c2cf9f1ec0e12df283e14d3573f82
bluewhale0528 <47441767+bluewhale0528@users.noreply.github.com>
2021-01-25 14:32:20 +0900

    Create 9012_정원.py

---
+++ w5.자료구조/BOJ_9012(괄호)/9012_정원.py
@@ -0,0 +1,24 @@
+# 28776KB	64ms
+from sys import stdin
+
+def solve(list):
+    # 비어있는 스택 생성
+		stack = []
+    
+		for x in list:
+        if x == "(":
+            stack.append(x)
+        else:
+            try:
+                stack.pop()
+						# 스택이 비어있어서 pop할 수 없는 경우 (예외처리)
+            except:
+                return "NO"
+
+    return "YES" if len(stack) == 0 else "NO"
+
+
+for _ in range(int(stdin.readline())):
+    ls = [x for x in stdin.readline().rstrip()]
+
+    print(solve(ls))
